792164dfffc8febd27844df1d50026cec4f95942
DamianoSpampinato <ghgg585@gmail.com>
2024-04-10 15:34:14 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script>
+export default{
+  
+}
+</script>
+
+<template>
+  <div>
+    <h1>LA MIA PRIMA APP</h1>
+  </div>
+</template>
+
+<style>
+div{
+  width: 600px;
+  margin: 0 auto;
+  height: 500px;
+  background-image: url('./assets/Damiano.png');
+  background-position: center;
+  background-repeat: no-repeat;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: blue;
+}
+</style>
